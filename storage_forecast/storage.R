e1130fa09b973acfdb907b52a14fcadbbbe5731a
karlarao <karlarao@gmail.com>
2016-09-13 19:34:27 -0400

    added headroom threshold parameter

--- storage_forecast/storage.R
+++ storage_forecast/storage.R
@@ -8,6 +8,10 @@ library(dplyr)
 library(lubridate)
 library(tseries)
 
+# set capacity and threshold
+capacity=370000
+threshold=80
+
 # my function to set current directory
 set_current_directory()
 
@@ -38,14 +42,28 @@ saveWorkbook(workbook, file = "storage2.xlsx")
 e <- tslm(xdata3 ~ trend + I(trend^2) + I(sin(2*pi*trend/12)) + I(cos(2*pi*trend/12)))
 f <- forecast(e,h=length(xdata3), level=FALSE)
 GB <- append(xdata$Value,f$mean)
-
-# plot 
-capacity=370000
 # Time <- seq(as.Date(min(xdata$Date)), by = "days", length = length(xdata3)*2)  # create seq of dates
 Time <- seq(min(xdata$Date), by = "hours", length = length(xdata3)*2) # create seq of hours
-plot(Time,GB, main=paste0("Storage (", month(min(xdata$Date),label=TRUE,abbr=FALSE),")"), ylim=c(0,capacity*2))
+forecast_data <- data.frame(Time,GB)
+forecast_data_threshold <- tail(subset(forecast_data, forecast_data$GB > capacity*((threshold-1)/100) & forecast_data$GB < capacity*(threshold/100) ),n=1)
+
+
+#plot
+options(scipen=10)
+plot(forecast_data, main=paste0("Storage (", month(min(xdata$Date),label=TRUE,abbr=FALSE),")"), ylim=c(0,capacity*2),las=2,cex.axis=.6)
 # axis(1, 1:length(xdata3)*2,  cex.axis = .7)   # just number index on x axis
 abline(h=capacity, col="red", lty=2)
+abline(h=as.numeric(forecast_data_threshold[2]), col="orange", lty=2)
 abline(v=max(xdata$Date), col="blue")
-legend("topleft",legend=c(paste0("Capacity (", capacity, "GB)"),paste0("Current Day ", as.Date(max(xdata$Date))), paste0("(Start Day ", as.Date(min(xdata$Date)) ,")") ),col=c("red","blue"), lty=c(2,1,0),  pt.cex=1, cex=.6)
+legend("topleft",
+       legend=c(
+         paste0("(Start Day ", as.Date(min(xdata$Date)) ,")"),
+         paste0("Current Day ", as.Date(max(xdata$Date))), 
+         paste0("Capacity (", capacity, "GB)"), 
+         paste0(threshold,"% Capacity on ",as.character(format(forecast_data_threshold[1],"%Y-%m-%d")))
+       ),
+       col=c("","blue","red","orange"), 
+       lty=c(0,1,2,2),  
+       pt.cex=1, 
+       cex=.5)
 
